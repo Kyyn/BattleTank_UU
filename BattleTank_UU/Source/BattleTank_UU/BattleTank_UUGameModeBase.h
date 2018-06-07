6350de88bbfa671144a58e10dedd9cb3732b6e92
DESKTOP-681L4K3\robfe <orobfer@gmail.com>
2018-06-07 20:12:08 +0200

    BT Section 106 End

---
+++ BattleTank_UU/Source/BattleTank_UU/BattleTank_UUGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTank_UUGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_UU_API ABattleTank_UUGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
